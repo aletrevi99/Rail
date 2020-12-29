9cc52d951bde876a1c50b6e7d9bfcccd26cf5034
aletrevi99 <alessandrotrevisan99@gmail.com>
2020-12-29 19:43:55 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "Regional_Train.h"
+#include "High_Speed_Train.h"
+#include "Super_High_Speed_Train.h"
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
