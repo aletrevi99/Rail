3f552bbffbea7d3db26914fae0e445e632c1207a
AndreaGhi8 <76681742+AndreaGhi8@users.noreply.github.com>
2020-12-30 11:12:37 +0100

    Station

--- Include/Station/Station.h
+++ Include/Station/Station.h
@@ -18,14 +18,41 @@ class Station{
     public:
         Station(const Station&) = delete;
         Station& operator =(const Station&) = delete;
+        
+        std::string get_Station_name() const {return name;};
+        int get_Station_distance() const {return distance;};
+        bool get_Station_type() const {return is_Local;};
+        
     
     protected:
-        Station(std::string n, int d);
-        ~Station();
+        Station(std::string n, int d, bool loc)
+            : name {n}, distance {d}, is_Local {loc}, st1 {true}, st2 {true}, st3 {true}, st4 {true} {}
+        ~Station() = default;
     
     private:
         std::string name;
         int distance;
+        bool is_Local;      //is_Local = true -> stazione locale (true = 1, false = 0)
+        bool st1;
+        bool st2;
+        bool st3;
+        bool st4;
+    
+};
+
+class Principal_Station : public Station{
+    
+    public:
+        Principal_Station(std::string n, int d)
+            : Station(n, d, false){}
+    
+};
+
+class Local_Station : public Station{
+    
+    public:
+        Local_Station(std::string n, int d)
+            : Station(n, d, true){}
     
 };
 
