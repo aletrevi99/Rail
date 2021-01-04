780a42c8b970097308944c9290cc76a06155e349
aletrevi99 <alessandrotrevisan99@gmail.com>
2021-01-04 16:33:36 +0100

    added passedStations var and its getter/setter.

--- Include/Train/Train.h
+++ Include/Train/Train.h
@@ -3,38 +3,61 @@
 #ifndef ASSEGNAMENTO_2_TRAIN_H
 #define ASSEGNAMENTO_2_TRAIN_H
 
+#include <iostream>
 #include "vector"
+#include "../../Custom_Exceptions.h"
+
 
 class Train
 {
     public:
         // getter functions
         int getTrainCode() const;
+
         int getMaxSpeed() const;
+
         int getCurrSpeed() const;
+
         double getDistance() const;
+
         double getDelay() const;
+
         bool isDirection() const;
+
         int getBinary() const;
+
         bool isStopLocal() const;
+
         int getType() const;
+
         const std::vector<int> &getPath() const;
+
         int getMinutes() const;
+
         bool isDeposit() const;
+
+        int getPassedStations() const;
 
 
         // setter functions
         void setCurrSpeed(int cs);
+
         void setDistance(double d);
+
         void setBinary(int bin);
+
         void setPath(const std::vector<int> &path);
+
         void setMinutes(int minutes);
+
         void setDeposit(bool deposit);
+
+        void setPassedStations(int passedStations);
 
 
     protected:
         // costruttore
-        Train(int tc, bool dir, const std::vector<int>& p);
+        Train(int tc, bool dir, const std::vector<int> &p);
 
         // codice del treno
         const int trainCode;
@@ -58,29 +81,33 @@ class Train
         int type = 0;
         // variabile per indicare se un treno è nel deposito o no
         bool deposit = false;
+        // numero stazioni attraversate
+        int passedStations = 0;
         // vettore degli orari di arrivo
         std::vector<int> path;
 };
 
 // overload operatore < per ordinare i treni a seconda della loro velcità
-bool operator<(const Train&, const Train&);
+bool operator<(const Train &, const Train &);
+// overload operatore << per stampare un treno
+std::ostream& operator<<(std::ostream&, const Train&);
 
 class Regional_Train : public Train
 {
     public:
-        Regional_Train(int tc, bool dir, const std::vector<int>& p);
+        Regional_Train(int tc, bool dir, const std::vector<int> &p);
 };
 
 class High_Speed_Train : public Train
 {
     public:
-        High_Speed_Train(int tc, bool dir, const std::vector<int>& p);
+        High_Speed_Train(int tc, bool dir, const std::vector<int> &p);
 };
 
 class Super_High_Speed_Train : public Train
 {
     public:
-        Super_High_Speed_Train(int tc, bool dir, const std::vector<int>& p);
+        Super_High_Speed_Train(int tc, bool dir, const std::vector<int> &p);
 };
 
 #endif //ASSEGNAMENTO_2_TRAIN_H
