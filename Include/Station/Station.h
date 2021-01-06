0f32238ba837013f792f90ee547a693cf8069d95
AndreaGhi8 <76681742+AndreaGhi8@users.noreply.github.com>
2021-01-06 16:22:09 +0100

    Station aggiornato

--- Include/Station/Station.h
+++ Include/Station/Station.h
@@ -7,11 +7,10 @@
  * Data di creazione: 29/12/2020
 */
 
+#include <iostream>
+
 #ifndef ASSEGNAMENTO_2_STATION_H
 #define ASSEGNAMENTO_2_STATION_H
-
-#include <iostream>
-#include <utility>
 
 class Station{
     
@@ -26,22 +25,22 @@ class Station{
         bool get_stb3_status() const {return stb3;};
         bool get_stb4_status() const {return stb4;};
         
-        void set_Station_distance(int d) { distance = d;};
-        void set_stb1_status(bool s) { stb1 = s;};
-        void set_stb2_status(bool s) { stb2 = s;};
-        void set_stb3_status(bool s) { stb3 = s;};
-        void set_stb4_status(bool s) { stb4 = s;};
+        void set_Station_distance(int d) {distance = d;};
+        void set_stb1_status(bool s) {stb1 = s;};
+        void set_stb2_status(bool s) {stb2 = s;};
+        void set_stb3_status(bool s) {stb3 = s;};
+        void set_stb4_status(bool s) {stb4 = s;};
     
     protected:
         Station(std::string n, int d, bool loc)
-            : name {std::move(n)}, distance {d}, is_Local {loc}, stb1 {true}, stb2 {true}, stb3 {true}, stb4 {true} {};
+            : name {n}, distance {d}, is_Local {loc}, stb1 {true}, stb2 {true}, stb3 {true}, stb4 {true} {};
         //tolto il distruttore
     
     private:
         std::string name;
         int distance;
         bool is_Local;      //is_Local = true -> stazione locale (true = 1, false = 0)
-        bool stb1;          //stb -> standard binary, ture  = binario libero
+        bool stb1;          //stb -> standard binary, true  = binario libero
         bool stb2;
         bool stb3;
         bool stb4;
@@ -52,7 +51,7 @@ class Principal_Station : public Station{
     
     public:
         Principal_Station(std::string n, int d)
-            : Station(std::move(n), d, false){}
+            : Station(n, d, false){}
     
 };
 
@@ -60,7 +59,7 @@ class Local_Station : public Station{
     
     public:
         Local_Station(std::string n, int d)
-            : Station(std::move(n), d, true){}
+            : Station(n, d, true){}
     
 };
 
