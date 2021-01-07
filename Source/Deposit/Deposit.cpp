4d860111563c52605dcf183847901a9b536ed2b2
aletrevi99 <alessandrotrevisan99@gmail.com>
2021-01-07 18:26:59 +0100

    Deposit with priority queue

--- Source/Deposit/Deposit.cpp
+++ Source/Deposit/Deposit.cpp
@@ -2,58 +2,62 @@
 
 #include "Deposit.h"
 
+/*
 // tre code code starnard specializzate
-void Deposit::addTrain(Regional_Train *t)
+void Deposit::addTrain(Regional_Train t)
 {
     rq.push(t);
 }
 
-void Deposit::addTrain(High_Speed_Train *t)
+void Deposit::addTrain(High_Speed_Train t)
 {
     hq.push(t);
 }
 
-void Deposit::addTrain(Super_High_Speed_Train *t)
+void Deposit::addTrain(Super_High_Speed_Train t)
 {
     sq.push(t);
 }
+*/
 
 // coda prioritaria
-void Deposit::addATrain(Train *t)
+void Deposit::addTrain(const Train& t)
 {
     pq.push(t);
 }
 
 // coda prioritaria
-Train *Deposit::deleteTrain()
+Train Deposit::removeTrain()
 {
-    Train *tmp = pq.top();
+    Train tmp = pq.top();
     pq.pop();
     return tmp;
 }
 
+/*
 // tre code standard specializzate
-Train *Deposit::removeTrain()
+Train Deposit::deleteTrain()
 {
     if (sq.empty())
     {
         if (hq.empty())
         {
-            Regional_Train *tmp = rq.front();
+            Regional_Train tmp = rq.front();
             rq.pop();
             return tmp;
         } else
         {
-            High_Speed_Train *tmp = hq.front();
+            High_Speed_Train tmp = hq.front();
             hq.pop();
             return tmp;
         }
     } else
     {
-        Super_High_Speed_Train *tmp = sq.front();
+        Super_High_Speed_Train tmp = sq.front();
         sq.pop();
         return tmp;
     }
 }
+*/
 
 //Alessandro Trevisan 1221819
